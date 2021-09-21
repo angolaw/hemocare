f8a378b8930967ecad63a66933ad42558ff2e507
Willian S <wsapcf@gmail.com>
2021-09-21 05:35:43 -0300

    IntroSlider initial definition

---
+++ lib/presentation/journeys/intro_slider/intro_slider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+
+class CustomIntroSlider extends StatefulWidget {
+  const CustomIntroSlider({Key? key}) : super(key: key);
+
+  @override
+  _CustomIntroSliderState createState() => _CustomIntroSliderState();
+}
+
+class _CustomIntroSliderState extends State<CustomIntroSlider> {
+  List<Slide> slides = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    slides.add(Slide(
+      title: 'Hemocare',
+      styleTitle: GoogleFonts.amaticaSc(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+      description:
+          'Hemocare é um app que permite o paciente realizar o controle de infusões',
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: IntroSlider(
+        slides: slides,
+        showDotIndicator: true,
+        colorDot: const Color(0xff7159c1),
+      ),
+    );
+  }
+}
