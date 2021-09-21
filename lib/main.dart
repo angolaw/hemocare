8891c8ac19a9b07c0cb334c37f9c81a3d3b03984
Willian S <wsapcf@gmail.com>
2021-09-21 05:53:46 -0300

    remove intro_slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hemocare/presentation/journeys/intro_slider/intro_slider.dart';
 
 void main() {
   runApp(CustomIntroSlider());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.notoSans().fontFamily,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0),
+        ),
+      ),
+    );
+  }
 }
